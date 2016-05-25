ef739c12284a1b6cc64d8d858162366799e50f25
Ivan <hladysh.im@gmail.com>
2016-05-25 12:10:55 +0000

    created menu + video backgraund + map

---
+++ js/common.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    var touch = $('#touch_menu');
+    var menu = $('.nav');
+
+    $(touch).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(window).resize(function() {
+        var wid = $(window).width();
+        if (wid > 760 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    })
+});
+
+
+
+
+(function($) {
+    $(document).ready(function() {
+
+    /* IF YOU WANT TO APPLY SOME BASIC JQUERY TO REMOVE THE VIDEO BACKGROUND ON A SPECIFIC VIEWPORT MANUALLY
+
+     var is_mobile = false;
+
+    if( $('.player').css('display')=='none') {
+        is_mobile = true;       
+    }
+    if (is_mobile == true) {
+        //Conditional script here
+        $('.big-background, .small-background-section').addClass('big-background-default-image');
+    }else{
+        $(".player").mb_YTPlayer(); 
+    }
+
+    });
+
+*/
+    /*  IF YOU WANT TO USE DEVICE.JS TO DETECT THE VIEWPORT AND MANIPULATE THE OUTPUT  */
+
+        //Device.js will check if it is Tablet or Mobile - http://matthewhudson.me/projects/device.js/
+        if (!device.tablet() && !device.mobile()) {
+            $(".player").mb_YTPlayer();
+        } else {
+            //jQuery will add the default background to the preferred class 
+            $('.big-background, .small-background-section').addClass(
+                'big-background-default-image');
+        }
+    });
+
+
+})(jQuery);
+
+
